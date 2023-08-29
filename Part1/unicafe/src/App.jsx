c2da82df9decebd79beaf45f638a6121d22a069f
Tirth Patel <tirthpatelcse@gmail.com>
2023-08-29 16:58:22 +0530

    Exercise 1.8 Refactor and added Statistics component | unicafe

--- Part1/unicafe/src/App.jsx
+++ Part1/unicafe/src/App.jsx
@@ -1,24 +1,36 @@
 import { useState } from 'react'
 
-const App = () => {
-  // save clicks of each button to its own state
-  const [good, setGood] = useState(6)
-  const [neutral, setNeutral] = useState(2)
-  const [bad, setBad] = useState(1)
-
+const Statistics = ({good, neutral, bad}) => {
   return (
-    <div>
-      <h1>give feedback</h1>
-      <button>good</button>
-      <button>neutral</button>
-      <button>bad</button>
+    <>
       <h1>statistics</h1>
       <p>good {good}</p>
       <p>neutral {neutral}</p>
       <p>bad {bad}</p>
       <p>all {good + neutral + bad}</p>
-      <p>average {((good*1)+(bad*-1))/9}</p>
-      <p>positive {good/(good+neutral+bad)*100}%</p>
+      <p>average {(good - bad)/(good+neutral+bad)}</p>
+      <p>positive {good!=0 ? good/(good+neutral+bad)*100 : 0}%</p>
+    </>
+  )
+}
+
+const App = () => {
+  // save clicks of each button to its own state
+  const [good, setGood] = useState(0)
+  const [neutral, setNeutral] = useState(0)
+  const [bad, setBad] = useState(0)
+
+  const handleGoodClick = () => setGood(good+1);
+  const handleNeutralClick = () => setNeutral(neutral+1);
+  const handleBadClick = () => setBad(bad+1);
+
+  return (
+    <div>
+      <h1>give feedback</h1>
+      <button onClick={handleGoodClick}>good</button>
+      <button onClick={handleNeutralClick}>neutral</button>
+      <button onClick={handleBadClick}>bad</button>
+      <Statistics good={good} neutral={neutral} bad={bad}/>
     </div>
   )
 }
