3d65244f62610fa04ff1366e2e6e7835850d5ad7
Tirth Patel <tirthpatelcse@gmail.com>
2023-09-03 00:28:40 +0530

    Exercise 2.9 Implemented search to search persons by name | phonebook

--- Part2/phonebook/src/App.jsx
+++ Part2/phonebook/src/App.jsx
@@ -2,10 +2,14 @@ import { useState } from 'react'
 
 const App = () => {
   const [persons, setPersons] = useState([
-    { name: 'Arto Hellas', number: '040-1234567' }
+    { name: 'Arto Hellas', number: '040-123456', id: 1 },
+    { name: 'Ada Lovelace', number: '39-44-5323523', id: 2 },
+    { name: 'Dan Abramov', number: '12-43-234345', id: 3 },
+    { name: 'Mary Poppendieck', number: '39-23-6423122', id: 4 }
   ]) 
   const [newName, setNewName] = useState('')
   const [newNumber, setNewNumber] = useState('')
+  const [searchQuery, setSearchQuery] = useState('')
 
   const handleNameChange = (event) => {
     console.log(event.target.value);
@@ -23,27 +27,37 @@ const App = () => {
     if(isPersonAdded) {
       return alert(`${newName} is already added to phonebook`)
     }
-    setPersons(persons.concat({ name: newName, number: newNumber }))
+    setPersons(persons.concat({ name: newName, number: newNumber, id: persons.length }))
     setNewName('')
     setNewNumber('')
   }
 
+  const personsToDisplay = searchQuery === '' 
+    ? persons 
+    : persons.filter((person) => 
+        person.name.toLocaleLowerCase().includes(searchQuery.toLocaleLowerCase())
+      )
+
   return (
     <div>
       <h2>Phonebook</h2>
+      <div>
+        filter shown with <input value={searchQuery} onChange={(event) => setSearchQuery(event.target.value)}/>
+      </div>
+      <h2>add a new</h2>
       <form onSubmit={addName}>
         <div>
           name: <input value={newName} onChange={handleNameChange} />
         </div>
         <div>
-          number: <input value={newNumber} onChange={handleNumberChange}/>
+          number: <input value={newNumber} onChange={handleNumberChange} />
         </div>
         <div>
           <button type="submit">add</button>
         </div>
       </form>
       <h2>Numbers</h2>
-      {persons.map((person) => <div key={person.name}>{person.name} {person.number}</div>)}
+      {personsToDisplay.map((person) => <div key={person.id}>{person.name} {person.number}</div>)}
     </div>
   )
 }
